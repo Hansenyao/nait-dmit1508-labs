446aa48f2a072bccd673ba9239ea48a6f9031f33
Youfang Yao <yyfsingler@163.com>
2024-07-19 09:47:08 -0700

    update sql

--- Lab3/Lab3Solution.sql
+++ Lab3/Lab3Solution.sql
@@ -35,10 +35,14 @@ GO
 CREATE PROCEDURE AddStaffType (@TypeDescription VARCHAR(30) = NULL)
 AS
 BEGIN
+	DECLARE @Error INT
+	DECLARE @RowCount INT
+	DECLARE @Identity INT
+
 	/* Check parameters for stored procedure */
 	IF @TypeDescription IS NULL
 		BEGIN
-			RaisError('You must privode a staff type description!', 16, 1)
+			RaisError('You must provide a staff type description!', 16, 1)
 		END
 	ELSE
 		BEGIN
@@ -50,19 +54,32 @@ BEGIN
 			ELSE
 				BEGIN
 					/* Insert the new type description to tablt StaffType */
-					INSERT INTO StaffType (StaffTypeDescription) Values (@TypeDescription)
-					/* Select the type id of new record */
-					SELECT StaffType.StaffTypeID FROM StaffType WHERE StaffType.StaffTypeDescription = @TypeDescription
+					INSERT INTO StaffType 
+						(StaffTypeDescription) 
+					VALUES 
+						(@TypeDescription)
+					SELECT @Error = @@ERROR, @RowCount=@@ROWCOUNT, @Identity = @@IDENTITY
+					IF @Error <> 0
+						BEGIN
+							RaisError('Insert a new record into table StaffType failed!', 16, 3)
+						END
+					ELSE IF @RowCount = 0
+						BEGIN
+							RaisError('You did not insert any record into table StaffType!', 16, 4)
+						END
+					ELSE
+						/* Select the type id of new record */
+						BEGIN
+							SELECT @Identity AS 'New Staff Type ID'
+						END
 				END
 		END
 END
 RETURN
 GO
 -- Test
-EXEC AddStaffType
-EXEC AddStaffType 'Sales'
-EXEC AddStaffType 'aaaddSalesss'
-GO
+--EXEC AddStaffType 'CCC'
+--GO
 
 -- =====================================================================================================================
 -- 2.	Write a stored procedure called UpdateReward that accepts the RewardID, Description and DiscountPercentage.
@@ -73,18 +90,21 @@ CREATE PROCEDURE UpdateReward(@RewardID CHAR(4) = NULL,
 							  @DiscountPercentage TINYINT = NULL)
 AS
 BEGIN
+	DECLARE @Error INT
+	DECLARE @RowCount INT
+
 	/* Check parameters for stored procedure */
 	IF @RewardID IS NULL 
 		BEGIN
-			RaisError('You must privode the reward ID value!', 16, 1)
+			RaisError('You must provide the reward ID value!', 16, 1)
 		END
 	ELSE IF  @Description IS NULL 
 		BEGIN
-			RaisError('You must privode the reward description!', 16, 2)
+			RaisError('You must provide the reward description!', 16, 2)
 		END
 	ELSE IF @DiscountPercentage IS NULL 
 		BEGIN
-			RaisError('You must privode the reward discount percentage value!', 16, 3)
+			RaisError('You must provide the reward discount percentage value!', 16, 3)
 		END
 	ELSE
 		BEGIN
@@ -99,15 +119,23 @@ BEGIN
 					UPDATE Reward
 						SET Reward.RewardDescription = @Description, Reward.DiscountPercentage = @DiscountPercentage
 					WHERE Reward.RewardID = @RewardID
+					SELECT @Error = @@ERROR, @RowCount = @@ROWCOUNT
+					IF @Error <> 0
+						BEGIN
+							RaisError('Update records in table Reward failed!', 16, 5)
+						END
+					ELSE IF @RowCount = 0
+						BEGIN
+							RaisError('You did not update any record in table Reward!', 16, 6)
+						END
 				END
 		END
 END
 RETURN
 Go
 -- Test
-EXEC UpdateReward
-EXEC UpdateReward 'A00', 'bbb', 10
-GO
+--EXEC UpdateReward 'A000', 'BBBB', 20
+--GO
 
 -- =====================================================================================================================
 -- 3.	Write a stored procedure called DeleteConsignmentDetail that will delete a single ConsignmentDetail record.
@@ -117,14 +145,17 @@ CREATE PROCEDURE DeleteConsignmentDetail (@ConsignmentID INT = NULL,
 										  @LineID INT = NULL)
 AS
 BEGIN
+	DECLARE @Error INT
+	DECLARE @RowCount INT
+
 	/* Check parameters for stored procedure */
 	IF @ConsignmentID IS NULL 
 		BEGIN
-			RaisError('You must privode the consignment ID value!', 16, 1)
+			RaisError('You must provide the consignment ID value!', 16, 1)
 		END
-	ELSE IF  @LineID IS NULL 
+	ELSE IF @LineID IS NULL 
 		BEGIN
-			RaisError('You must privode the line ID value!', 16, 2)
+			RaisError('You must provide the line ID value!', 16, 2)
 		END
 	ELSE
 		BEGIN
@@ -139,13 +170,23 @@ BEGIN
 				BEGIN
 					DELETE ConsignmentDetail
 					WHERE ConsignmentDetail.ConsignmentID = @ConsignmentID AND ConsignmentDetail.LineID = @LineID
+					SELECT @Error = @@ERROR, @RowCount = @@ROWCOUNT
+					IF @Error <> 0 
+						BEGIN
+							RaisError('Delete the record from table ConsignmentDetail failed!', 16, 4)
+						END
+					ELSE IF @RowCount = 0
+						BEGIN
+							RaisError('You did not delete any record from table ConsignmentDetail!', 16, 5)
+						END
 				END
 		END
 END
 RETURN
 GO
 -- Test
-EXEC DeleteConsignmentDetail 1, 1
+--EXEC DeleteConsignmentDetail 1, 1
+--GO
 
 -- =====================================================================================================================
 -- 4.	Write a stored procedure called LookUpCustomerHistory that will return the Customer Full Name, Consignment
@@ -158,7 +199,7 @@ BEGIN
 	/* Check parameters for stored procedure */
 	IF @FirstName IS NULL OR @LastName IS NULL
 		BEGIN
-			RaisError('You must privode a customer name!', 16, 1)
+			RaisError('You must provide a customer name!', 16, 1)
 		END
 	ELSE
 		BEGIN
@@ -166,7 +207,7 @@ BEGIN
 			IF NOT EXISTS (SELECT * FROM Customer 
 						   WHERE Customer.FirstName = @FirstName AND Customer.LastName = @LastName)
 				BEGIN
-					RaisError('This customer: %s %s does not exists!', 16, 3,  @FirstName, @LastName)
+					RaisError('This customer: %s %s does not exists!', 16, 2,  @FirstName, @LastName)
 				END
 			/* Otherwise, select the consignment history items for this customer */
 			ELSE
@@ -187,18 +228,20 @@ END
 RETURN
 GO
 -- Test
-EXEC LookUpCustomerHistory
-EXEC LookUpCustomerHistory 'aa', 'bb'
-EXEC LookUpCustomerHistory 'Fred', 'Flinstone'
-EXEC LookUpCustomerHistory 'Homer', 'Simpson'
-EXEC LookUpCustomerHistory 'Luke', 'Skywalker'
-EXEC LookUpCustomerHistory 'Sue', 'Sampson'
-GO
+--EXEC LookUpCustomerHistory
+--EXEC LookUpCustomerHistory 'aa', 'bb'
+--EXEC LookUpCustomerHistory 'Fred', 'Flinstone'
+--EXEC LookUpCustomerHistory 'Homer', 'Simpson'
+--EXEC LookUpCustomerHistory 'Luke', 'Skywalker'
+--EXEC LookUpCustomerHistory 'Sue', 'Sampson'
+--GO
 
 -- =====================================================================================================================
 -- 5.	Write a stored procedure called UnusedCategories that returns the CategoryCode and CategoryDescription of
 --      all the Categories that have not been used on any consignments. Do not use a join.
 --      (2 Marks)
+CREATE PROCEDURE UnusedCategories
+AS
 BEGIN
 	SELECT Category.CategoryCode, Category.CategoryDescription
 	FROM Category
@@ -208,8 +251,8 @@ END
 RETURN
 GO
 -- Test
-EXEC UnusedCategories
-GO
+--EXEC UnusedCategories
+--GO
 
 -- =====================================================================================================================
 -- 6.	Write a stored procedure called LookUpStaffType that accepts any part of a Staff Type Description to search
@@ -221,20 +264,19 @@ BEGIN
 	/* Check parameters for stored procedure */
 	IF @PartDescription IS NULL
 		BEGIN
-			RaisERROR('You must privode a part of staff type description!', 16, 1)
+			RaisERROR('You must provide a part of staff type description!', 16, 1)
 		END
 	ELSE
 		BEGIN
-			SELECT *
-			FROM StaffType
+			SELECT * FROM StaffType
 			WHERE StaffType.StaffTypeDescription LIKE '%' + @PartDescription + '%'
 		END
 END
 RETURN
 GO
 -- Test
-EXEC LookUpStaffType 'Re'
-GO
+--EXEC LookUpStaffType 'Re'
+--GO
 
 -- =====================================================================================================================
 -- 7.	Write a stored procedure called AddConsignmentItem that will accept ConsignmentID, Description, StartPrice,
@@ -260,26 +302,29 @@ BEGIN
 	/* Check parameters for stored procedure */
 	IF @ConsignmentID IS NULL
 		BEGIN
-			RaisError('You must privode a ConsignmentID value!', 16, 1)
+			RaisError('You must provide a ConsignmentID value!', 16, 1)
 		END
 	ELSE IF @Description IS NULL
 		BEGIN
-			RaisError('You must privode a Description value!', 16, 2)
+			RaisError('You must provide a Description value!', 16, 2)
 		END
 	ELSE IF @StartPrice IS NULL
 		BEGIN
-			RaisError('You must privode a StartPrice value!', 16, 3)
+			RaisError('You must provide a StartPrice value!', 16, 3)
 		END
 	ELSE IF @LowPrice IS NULL
 		BEGIN
-			RaisError('You must privode a LowPrice value!', 16, 4)
+			RaisError('You must provide a LowPrice value!', 16, 4)
 		END
 	ELSE IF @CategoryCode IS NULL
 		BEGIN
-			RaisError('You must privode a CategoryCode value!', 16, 5)
+			RaisError('You must provide a CategoryCode value!', 16, 5)
 		END
 	ELSE
 		BEGIN
+			DECLARE @Error INT
+			DECLARE @RowCount INT
+
 			/* Check @ConsignmentID exists in table Consignment or not */
 			IF NOT EXISTS (SELECT * FROM Consignment WHERE Consignment.ConsignmentID = @ConsignmentID)
 				BEGIN
@@ -293,95 +338,123 @@ BEGIN
 					RETURN
 				END
 			/* Get the line ID for the new record */
-			DECLARE @LineID AS INT
+			DECLARE @LineID INT
 			IF NOT EXISTS (SELECT * FROM ConsignmentDetail WHERE ConsignmentDetail.ConsignmentID = @ConsignmentID)
 				BEGIN
 					SET @LineID = 1
 				END
 			ELSE
 				BEGIN
-					SELECT @LineID = MAX(ConsignmentDetail.LineID) + 1 FROM ConsignmentDetail WHERE ConsignmentDetail.ConsignmentID = @ConsignmentID
+					SELECT @LineID = MAX(ConsignmentDetail.LineID) + 1 
+					FROM ConsignmentDetail 
+					WHERE ConsignmentDetail.ConsignmentID = @ConsignmentID
 				END
+
 			/* Use transcation to update tables ConsignmentDetail and Consignment integrity */
 			BEGIN TRANSACTION
-				/* At first, try to insert the new recor into table ConsignmentDetail*/
-				INSERT INTO ConsignmentDetail
-					(ConsignmentID, LineID, ItemDescription, StartPrice, LowestPrice, CategoryCode)
-				VALUES
-					(@ConsignmentID, @LineID, @Description, @StartPrice, @LowPrice, @CategoryCode)
-				IF @@ERROR <> 0
-					BEGIN
-						RaisError('Insert this new record to table ConsignmentDetail failed!', 16, 8)
-						ROLLBACK TRANSACTION
-					END
-				/* Then, update the relevant record table Consignment */
-				ELSE
-					BEGIN
-						/* Get the discount percent for this customer */
-						DECLARE @DiscountPercent AS TINYINT
-						SELECT @DiscountPercent = Reward.DiscountPercentage 
-						FROM Reward
-							INNER JOIN Customer
-							ON Reward.RewardID = Customer.RewardID
-							INNER JOIN Consignment
-							ON Customer.CustomerID = Consignment.CustomerID
-						WHERE Consignment.ConsignmentID = @ConsignmentID
-						IF @@ERROR <> 0
-							BEGIN
-								RaisError('Search reward discount percent for this customer failed!', 16, 9)
-								ROLLBACK TRANSACTION
-								RETURN
-							END
+			/* At first, try to insert the new recor into table ConsignmentDetail*/
+			INSERT INTO ConsignmentDetail
+				(ConsignmentID, LineID, ItemDescription, StartPrice, LowestPrice, CategoryCode)
+			VALUES
+				(@ConsignmentID, @LineID, @Description, @StartPrice, @LowPrice, @CategoryCode)
+			SELECT @Error = @@ERROR, @RowCount = @@ROWCOUNT
+			IF @Error <> 0
+				BEGIN
+					ROLLBACK TRANSACTION
+					RaisError('Insert this new record to table ConsignmentDetail failed!', 16, 8)
+				END
+			ELSE IF @RowCount = 0
+				BEGIN
+					ROLLBACK TRANSACTION
+					RaisError('You did not insert any new record to table ConsignmentDetail!', 16, 9)
+				END
+			/* Then, update the relevant record table Consignment */
+			ELSE
+				BEGIN
+					/* Get the discount percent for this customer */
+					DECLARE @DiscountPercent TINYINT
+					SELECT @DiscountPercent = Reward.DiscountPercentage 
+					FROM Reward
+						INNER JOIN Customer
+						ON Reward.RewardID = Customer.RewardID
+						INNER JOIN Consignment
+						ON Customer.CustomerID = Consignment.CustomerID
+					WHERE Consignment.ConsignmentID = @ConsignmentID
+					SELECT @Error = @@ERROR, @RowCount = @@ROWCOUNT
+					IF @Error <> 0
+						BEGIN
+							ROLLBACK TRANSACTION
+							RaisError('Search reward discount percent for this customer failed!', 16, 10)
+							RETURN
+						END
+					ELSE IF @RowCount = 0
+						BEGIN
+							ROLLBACK TRANSACTION
+							RaisError('No any reward discount percent record for this customer!', 16, 11)
+							RETURN
+						END
 						
-						/* Get the new SubTotal and discount for this item */
-						DECLARE @NewDisCount AS SMALLMONEY
-						DECLARE @NewSubTotal AS SMALLMONEY						
-						SELECT @NewSubTotal = SUM(Category.Cost) 
-						FROM ConsignmentDetail
-							INNER JOIN Category
-							ON ConsignmentDetail.CategoryCode = Category.CategoryCode
-						WHERE ConsignmentDetail.ConsignmentID = @ConsignmentID
-						IF @@ERROR <> 0
-							BEGIN
-								RaisError('Search subtotals for this consignment failed!', 16, 10)
-								ROLLBACK TRANSACTION
-								RETURN
-							END
-						ELSE
-							BEGIN
-								SET @NewDisCount = @NewSubTotal * (@DiscountPercent / 100.0)
-								SET @NewSubTotal = @NewSubTotal - @NewDisCount
-							END
+					/* Get the new SubTotal and discount for this item */
+					DECLARE @NewDisCount SMALLMONEY
+					DECLARE @NewSubTotal SMALLMONEY						
+					SELECT @NewSubTotal = SUM(Category.Cost) 
+					FROM ConsignmentDetail
+						INNER JOIN Category
+						ON ConsignmentDetail.CategoryCode = Category.CategoryCode
+					WHERE ConsignmentDetail.ConsignmentID = @ConsignmentID
+					SELECT @Error = @@ERROR, @RowCount = @@ROWCOUNT
+					IF @Error <> 0
+						BEGIN
+							ROLLBACK TRANSACTION
+							RaisError('Search subtotals for this consignment in table ConsignmentDetail failed!', 16, 12)
+							RETURN
+						END
+					ELSE IF @RowCount = 0
+						BEGIN
+							ROLLBACK TRANSACTION
+							RaisError('No any detail record in table ConsignmentDetail for this consignment!', 16, 13)
+							RETURN
+						END
+					ELSE
+						BEGIN
+							SET @NewDisCount = @NewSubTotal * (@DiscountPercent / 100.0)
+							SET @NewSubTotal = @NewSubTotal - @NewDisCount
+						END
 
-						/* Calculate new GST and total */
-						DECLARE @NewGST AS SMALLMONEY
-						DECLARE @NewTotal AS SMALLMONEY
-						SET @NewGST = @NewSubTotal * 0.05
-						SET @NewTotal = @NewSubTotal + @NewGST
+					/* Calculate new GST and total */
+					DECLARE @NewGST SMALLMONEY
+					DECLARE @NewTotal SMALLMONEY
+					SET @NewGST = @NewSubTotal * 0.05
+					SET @NewTotal = @NewSubTotal + @NewGST
 
-						/* Update the relevant record */
-						UPDATE Consignment
-						SET Consignment.SubTotal = @NewSubTotal, 
-							Consignment.GST = @NewGST, 
-							Consignment.Total = @NewTotal, 
-							Consignment.RewardsDiscount = @NewDisCount,
-							Consignment.Date = GETDATE()
-						WHERE Consignment.ConsignmentID = @ConsignmentID
-						IF @@ERROR <> 0
-							BEGIN
-								RaisError('Update table Consignment failed!', 16, 11)
-								ROLLBACK TRANSACTION
-								RETURN
-							END
-						ELSE
-							BEGIN
-								COMMIT TRANSACTION
-							END
-					END
+					/* Update the relevant record */
+					UPDATE Consignment
+					SET Consignment.SubTotal = @NewSubTotal, 
+						Consignment.GST = @NewGST, 
+						Consignment.Total = @NewTotal, 
+						Consignment.RewardsDiscount = @NewDisCount
+					WHERE Consignment.ConsignmentID = @ConsignmentID
+					SELECT @Error = @@ERROR, @RowCount = @@ROWCOUNT
+					IF @Error <> 0
+						BEGIN
+							ROLLBACK TRANSACTION
+							RaisError('Update table Consignment failed!', 16, 14)
+						END
+					ELSE IF @RowCount = 0
+						BEGIN
+							ROLLBACK TRANSACTION
+							RaisError('No any record is updated in table Consignment failed!', 16, 15)
+						END
+					ELSE
+						BEGIN
+							/* Success! */
+							COMMIT TRANSACTION
+						END
+				END
 		END
 END
 RETURN
 GO
 -- Test
-EXEC AddConsignmentItem 6, 'Test', 10, 5, 'AAA'
-GO
+--EXEC AddConsignmentItem 6, 'Test', 10, 5, 'AAA'
+--GO
